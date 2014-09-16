e5a9145acfa82e228de70367bfdba06683669088
Remco <helium_2@hotmail.com>
2014-09-16 14:53:06 +0200

    ready for use

--- README.md
+++ README.md
@@ -1,41 +1,37 @@
-SVGPanZoom V0.9
+SVGPanZoom V1.0
 ==========
 
-Sweet, short and simple stand-alone SVG Pan Zoom code including support for autoscaled SVGs and touch events. Apache V2 license. 
+Sweet, short and simple stand-alone SVG Pan Zoom code including support for autoscaled SVGs and touch control (pinch to zoom). Apache V2 license. 
 
-Should work on: FF, Chrome, Chrome mobile, Android 4+, ...? 
+Should work on: IE9+, FF, Chrome, Chrome mobile, Android 4+, ...? 
 
 Tests (so far only inline svg followed by inline javascript):
-Working on: Opera Mobile, FF, Chrome, Chrome mobile
+Working on: Opera Mobile, FF, Chrome, Chrome mobile, IE11
 To be tested on: Opera mini (doesnt support inline SVG), Apple stuff
 
 This code is inspired on [SVGPan](https://code.google.com/p/svgpan/) by Andrea Leofreddi (also see [his blogpost](http://www.cyberz.org/blog/2009/12/08/svgpan-a-javascript-svg-panzoomdrag-library/)) and [code by Peter Collingridge](http://www.petercollingridge.co.uk/interactive-svg-components/pan-and-zoom-control). Their code is starting to get old, however, due to sketchy and sometimes quickly changing SVG browser implementations. Also, I needed support for autoscaled SVGs. 
 
-The code can be embedded in SVG image (dont forget to use <![CDATA[ ... ]]>) or anywhere else in the page. 
-Requirements:
-* The svg element and the panzoom group have IDs that match the ID in the code (should be easier in V1.0, see roadmap).
-* The panzoom group should have a matrix transform defined (Requirement should be dropped by V1.1, see roadmap).
+
+How to use:
+ * The code can be embedded in SVG image (dont forget to use <![CDATA[ ... ]]> in that case) or anywhere else in the page. 
+ * After the code is loaded, use addPanZoomToSVG({svgID: ..., panzoomID: ...,scrollSpeed: ..., pinchSpeed: ..., upOnMouseOut: ...}). All config is optional, default svgID and panzoomID are "svg" and "panzoom". 
+ * Make sure the svgID and panzoomID options are matching the ID tags in your svg elements. For svgID your svg tag is recommended, since it is used to attach the events to, and for the panzoomID a g tag can be used for example.
 
 Demo can be found at [my personal github page](http://remcotukker.github.io/SVGPanZoom/)
 
 ## Roadmap
 
-## V1.0
-
-* Clean up code a lil bit
-* Fix issue mouseout events
-* Make it a proper module with easy initialization of the IDs you want to use (and other config)
-* Touch up docu
-* Testing on browsers, both inline and in separate SVG, fix any incompatibility issues for main browsers
-
 ## V1.1
 
-* Use requestAnimationFrame when it is available
-* Add a minified version and/or a data-uri version
 * Add rotation, in particular attractive for mobile use
+* Make a proper distribution including a minified version, a data-uri version and a AMD module version
+* More testing, both inline and in separate SVG, fix any incompatibility issues for main browsers
+
+## V1.2
+
+* Use requestAnimationFrame when it is available
 
 ## V2.0
 
-* Drop IE support and allow ourselves to directly manipulate the transforms instead of relying on string parsing / concatenation
 * Allow for inertia-based pan/zooming (in particular for smooth zooming :-)
-* Test Apple browsers
+
--- SVGPanZoom.js
+++ SVGPanZoom.js
@@ -1,181 +1,134 @@
-    // SVG Pan/Zoom that can be used with autoscaled SVGs by Remco Tukker (2014)
-    // Distributed under a Apache V2 license
-    //
+// SVG Pan/Zoom that can be used with autoscaled SVGs by Remco Tukker (2014)
+// Distributed under a Apache V2 license
 
-    var svggraph = document.getElementById("svgtiger");
-    var svgpanzoom = svggraph.getElementById("tigerpanzoom");    
+var addPanZoomToSVG = function(config) {
 
-    // config
-    var zoomSensitivity = 5;   // speed of zooming
-    var ignoreMouseOut = true; // ignore mouse out or handle it as a mouseup event
-      // mouseout events are being generated when leaving -every- svg element; so, add test for evt.target == svg
-    
-    // get the currently used transformation; we have to do it this way only because of IE (see better alternative below)
-    var tfstring = svgpanzoom.getAttribute("transform");
-    if (!tfstring) tfstring = "matrix(1,0,0,1,0,0)";
-    var tfmatels = [];
-    tfstring = tfstring.slice( tfstring.indexOf("(") + 1 );
-    tfmatels[0] = parseFloat(tfstring);
-    for (var i = 1; i < 6; i++) {
-        tfstring = tfstring.slice( (tfstring.indexOf(",") + 1 ) || (tfstring.indexOf(" ") + 1 ) );
-        tfmatels[i] = parseFloat(tfstring);
+    /* ===== take care of config =====                                             */
+
+    var defaultConfig = {
+        svgID: svg,             // ID of the svg element that should catch mouse clicks and touches
+        panzoomID: panzoom,     // ID of the element that should be panzoomed
+        scrollSpeed: 5,         // Speed of scroll zooming
+        pinchSpeed: 1,          // Speed of pinch zooming (5 times smaller than scrollSpeed is an ok choice)
+        upOnMouseOut: false     // treat mouse leaving svg area as a mouseup event
     }
-    var tf = svggraph.createSVGMatrix(); 
-    tf.a = tfmatels[0]; tf.b = tfmatels[1]; tf.c = tfmatels[2]; tf.d = tfmatels[3]; tf.e = tfmatels[4]; tf.f = tfmatels[5]; 
-    
-    /* Alternative that is supported by all browsers except IE: (for V2... Maybe IE 12 will work? One can hope..)
-    var tf = svggraph.createSVGMatrix(); 
-    var transformList = svgpanzoom.transform.baseVal;
-    var panZoomTransform = transformList.createSVGTransformFromMatrix(tf);
-    transformList.appendItem(panZoomTransform); // add our own transform to the transforms that are already there
-    //panZoomTransform.setMatrix(tf);  // to update transform
-    */
+    config = config || {};
+    for (var key in defaultConfig) if (typeof config[key] == "undefined") config[key] = defaultConfig[key];
 
-    // mouse and touches state
-    var mousePressed = false, lastX, lastY;
+    // set up the panzoom transform we're going to use
+    var svg = document.getElementById(config.svgID);       
+    var panzoom = document.getElementById(config.panzoomID); 
+    panzoom.transform.baseVal.appendItem(panzoom.transform.baseVal.createSVGTransformFromMatrix(svg.createSVGMatrix()));
+    var tf = panzoom.transform.baseVal.getItem(panzoom.transform.baseVal.numberOfItems - 1);
+    var tfMatrix = tf.matrix;
+
+    // mouse and touches state; this object will hold all the locations of touches / mouse, accessed by a touch ID
     var ongoingTouches = {};
 
-    // convert screen-based coordinates (eg from mouse events) into the panzoom coordinate system
+
+    /* ===== functions for panning and zooming =====                               */
+
+    // Convert screen-based coordinates (eg from mouse events) into the panzoom coordinate system
     function screenToPanZoomCoords(x, y) {
-      var pt = svggraph.createSVGPoint();
+      var pt = svg.createSVGPoint();
       pt.x = x;
       pt.y = y;
-      return pt.matrixTransform(svgpanzoom.getScreenCTM().inverse());
+      return pt.matrixTransform(panzoom.getScreenCTM().inverse());
     }
 
-    // pan the image a certain x and y distance (in panzoom coordinates!)
-    function pan(x, y) {
-      tf = tf.translate(x, y);
-      svgpanzoom.setAttribute("transform", "matrix(" + tf.a + " " + tf.b + " " + tf.c + " " + tf.d + " " + tf.e + " " + tf.f + ")");
+    // pan the image a certain distance (from screen coords to screen coords)
+    function pan(xold, yold, xnew, ynew) {
+      var pt1 = screenToPanZoomCoords(xold, yold); // to make sure we always use the newest transform
+      var pt2 = screenToPanZoomCoords(xnew, ynew);
+      tfMatrix = tfMatrix.translate(pt2.x - pt1.x, pt2.y - pt1.y);
+      tf.setMatrix(tfMatrix);
     }
 
-    // zoom the image at a certain x y location (in panzoom coordinates!)
+    // zoom the image at a certain x y location (in screen coordinates)
     function zoom(x, y, factor) {  
-      // first translate x and y to origin, then zoom, then translate origin back to x y
-      tf = tf.translate(x, y).scale(factor).translate(-x, -y);
-      svgpanzoom.setAttribute("transform", "matrix(" + tf.a + " " + tf.b + " " + tf.c + " " + tf.d + " " + tf.e + " " + tf.f + ")");
+      var pt = screenToPanZoomCoords(x, y);
+      tfMatrix = tfMatrix.translate(pt.x, pt.y).scale(factor).translate(-pt.x, -pt.y); // first translate x and y to origin, then zoom, then translate origin back to x y
+      tf.setMatrix(tfMatrix);
     }
 
-    // functions for processing mouse movements
-    function mouseDown(evt) {
-      mousePressed = true;
-      lastX = evt.clientX;
-      lastY = evt.clientY;
-      //return false; // prevents browser from dragging elements in some situations
+
+    /* ===== functions for handling of events =====                                */
+
+    // Calculate average point of ongoingTouches and the total distance from that point to touches; ongoingTouches must not be empty! 
+    function touchLocationAndRadius() { 
+      var x = 0, y = 0, rsquared = 0;
+      for (var key in ongoingTouches) {
+          x += ongoingTouches[key].x;
+          y += ongoingTouches[key].y;
+      }
+      x /= Object.keys(ongoingTouches).length; y /= Object.keys(ongoingTouches).length;
+      for (var key in ongoingTouches) rsquared += (ongoingTouches[key].x - x) * (ongoingTouches[key].x - x) + (ongoingTouches[key].y - y) * (ongoingTouches[key].y - y);
+      return {x: x, y: y, rsquared: rsquared};
     }
 
-    function touchDown(evt) {
-      evt.preventDefault();
-
-      var touches = evt.changedTouches;
-      for (var i=0; i < touches.length; i++) {
-        ongoingTouches[touches[i].identifier] = {x: touches[i].clientX, y: touches[i].clientY};
-      } 
+    // mousedown / touchstart handling: add item to ongoingTouches object
+    function down(evt) {
+      if (!evt.changedTouches) evt.changedTouches = [{identifier: "mouse", clientX: evt.clientX, clientY: evt.clientY}]; // make mouse event look like touch event
+      for (var i=0; i < evt.changedTouches.length; i++) ongoingTouches[evt.changedTouches[i].identifier] = {x: evt.changedTouches[i].clientX, y: evt.changedTouches[i].clientY};
     }
 
-    function touchUp(evt) {
-      // remove touches from our list
-      evt.preventDefault();
-      var touches = evt.changedTouches;
-      for (var i=0; i < touches.length; i++) {
-        delete ongoingTouches[touches[i].identifier];
-      } 
-    }
+    // mousemove / touchmove handling
+    function move(evt) {
 
-    function touchMove(evt) {
-      evt.preventDefault();
-      var touches = Object.keys(ongoingTouches);
-      if (touches.length == 1) { // pan
-        var pt1 = screenToPanZoomCoords(ongoingTouches[touches[0]].x, ongoingTouches[touches[0]].y); // to make sure we always use the newest transform
-        var pt2 = screenToPanZoomCoords(evt.changedTouches[0].clientX, evt.changedTouches[0].clientY);
-        pan(pt2.x - pt1.x, pt2.y - pt1.y);
-        ongoingTouches[touches[0]].x = evt.changedTouches[0].clientX;
-        ongoingTouches[touches[0]].y = evt.changedTouches[0].clientY;
-      } else { // pinch-zoom
-        // find out last center and radius
-        var oldx = 0, oldy = 0, oldr = 0;
-        for (var key in ongoingTouches) {
-          oldx += ongoingTouches[key].x; // we'll divide by touches.length in the end
-          oldy += ongoingTouches[key].y;
-        }
-        oldx /= touches.length; oldy /= touches.length; // clearly, touches shouldnt be empty, lets see if we can break this
-        for (var key in ongoingTouches) {
-          oldr += (ongoingTouches[key].x - oldx) * (ongoingTouches[key].x - oldx) + (ongoingTouches[key].y - oldy) * (ongoingTouches[key].y - oldy);
-        }
-        console.log("old: " + oldx + "  " + oldy + "  " + oldr + " length " + touches.length);
-        // update touch info
-        for (var i=0; i < evt.changedTouches.length; i++) {
+      var touchIds = Object.keys(ongoingTouches);
+      if (touchIds.length == 0) return;  // no touches going on / mouse not pressed
+      evt.preventDefault();              // no scrolling of whole webpage with fingers
+
+      if (!evt.changedTouches) evt.changedTouches = [{identifier: "mouse", clientX: evt.clientX, clientY: evt.clientY}]; // make mouse look like finger
+
+      if (touchIds.length == 1) {   // just one finger or mouse => do panning
+        pan(ongoingTouches[touchIds[0]].x, ongoingTouches[touchIds[0]].y, evt.changedTouches[0].clientX, evt.changedTouches[0].clientY);
+        ongoingTouches[touchIds[0]].x = evt.changedTouches[0].clientX; // update touch info
+        ongoingTouches[touchIds[0]].y = evt.changedTouches[0].clientY;  
+
+      } else {                      // multiple fingers => do panning and zooming
+        var oldGesture = touchLocationAndRadius();            // get old location and radius
+        for (var i=0; i < evt.changedTouches.length; i++) {   // update touch info
           ongoingTouches[evt.changedTouches[i].identifier].x = evt.changedTouches[i].clientX;
           ongoingTouches[evt.changedTouches[i].identifier].y = evt.changedTouches[i].clientY;
         } 
+        var newGesture = touchLocationAndRadius();            // get new location and radius
 
-        // find out new center and radius
-        var newx = 0, newy = 0, newr = 0;
-        for (var key in ongoingTouches) {
-          newx += ongoingTouches[key].x; // we'll divide by touches.length in the end
-          newy += ongoingTouches[key].y;
-        }
-        newx /= touches.length; newy /= touches.length; // clearly, touches shouldnt be empty, lets see if we can break this
-        for (var key in ongoingTouches) {
-          newr += (ongoingTouches[key].x - newx) * (ongoingTouches[key].x - newx) + (ongoingTouches[key].y - newy) * (ongoingTouches[key].y - newy);
-        }
-         console.log("new: " + newx + "  " + newy + "  " + newr + " length " + Object.keys(ongoingTouches).length);
-        // pan and zoom
-        var pt1 = screenToPanZoomCoords(oldx, oldy); 
-        var pt2 = screenToPanZoomCoords(newx, newy);
-        pan(pt2.x - pt1.x, pt2.y - pt1.y);
-
-        var pt = screenToPanZoomCoords(newx, newy); // get newest coords
-        var z = Math.pow(newr / oldr, zoomSensitivity/2);         // TODO: this should be done a bit better, preferably similar to mouse
-        console.log(z);
-        zoom(pt.x, pt.y, z);                                      // and then zoom in
+        pan(oldGesture.x, oldGesture.y, newGesture.x, newGesture.y);               // first handle any panning
+        var z = Math.pow(newGesture.rsquared / oldGesture.rsquared, config.pinchSpeed);   // find out how much we want to zoom
+        zoom(newGesture.x, newGesture.y, z);                                       // and then zoom in
 
       }
-
-      
     }
 
-
-    function mouseUp(evt) {
-      mousePressed = false;
-    }
-
-    function mouseMove(evt) {
-      if (!mousePressed) return false; // only have to do something when mouse is pressed    
-
-      var pt1 = screenToPanZoomCoords(lastX, lastY); // to make sure we always use the newest transform
-      var pt2 = screenToPanZoomCoords(evt.clientX, evt.clientY)
-      pan(pt2.x - pt1.x, pt2.y - pt1.y);
-
-      lastX = evt.clientX;
-      lastY = evt.clientY;
-   
-      return false;
-    }
-
+    // mousewheel handling
     function wheel(evt) {
-      var pt = screenToPanZoomCoords(evt.clientX, evt.clientY); // find out location in panzoom coordinates
-      var z = 1 + (0.01 * evt.deltaY / Math.abs(evt.deltaY)) * zoomSensitivity;         // calculate how much we want to scale
-                                      // the Math.abs is to deal with some nasty browser differences in deltaY differences
-      
-      zoom(pt.x, pt.y, z);                                      // and then zoom in
-      evt.preventDefault();
-      return false; 
+      var z = 1 + (0.01 * evt.deltaY / Math.abs(evt.deltaY)) * config.scrollSpeed;         // calculate how much we want to scale
+                                // the Math.abs is to deal with some nasty browser differences in deltaY numbers
+      zoom(evt.clientX, evt.clientY, z);                                                // and then zoom in
+      evt.preventDefault();                                                             // prevent scrolling of page
     }
 
-    // last but not least, the necessary event listeners
-    svggraph.addEventListener('mousedown', function(evt){ return mouseDown(evt); });
-    svggraph.addEventListener('touchstart', function(evt){ return touchDown(evt); });
-    svggraph.addEventListener('mouseup', function(evt){ return mouseUp(evt); });
-    svggraph.addEventListener('touchend', function(evt){ return touchUp(evt); });
-    //svggraph.addEventListener('touchleave', function(evt){ return touchUp(evt); });
-    svggraph.addEventListener('touchcancel', function(evt){ return touchUp(evt); });
-    svggraph.addEventListener('mouseout', function(evt){ return ignoreMouseOut || mouseUp(evt); });
-    svggraph.addEventListener('mousemove', function(evt){ return mouseMove(evt); });
-    svggraph.addEventListener('touchmove', function(evt){ return touchMove(evt); });
+    // mouseup / touchend handling
+    function up(evt) {
+      if (!evt.changedTouches) evt.changedTouches = [{identifier: "mouse"}];            // make mouse look like touch
+      for (var i=0; i < evt.changedTouches.length; i++) delete ongoingTouches[evt.changedTouches[i].identifier]; 
+    }
 
-    // the "wheel" event is rather new, but if it doesnt work due to old browser, too bad, not important..
-    svggraph.addEventListener('wheel', function(evt){ return wheel(evt) });
 
-    svgpanzoom.addEventListener('dragstart', function(evt) {evt.preventDefault(); }); // no dragging away svg elements please
-    
+    /* ===== registering event handlers =====                                      */
+
+    svg.addEventListener('mousedown', down);
+    svg.addEventListener('touchstart', down);
+    svg.addEventListener('mouseup', up);
+    svg.addEventListener('touchend', up);
+    svg.addEventListener('touchcancel', up);
+    svg.addEventListener('mouseleave', function(evt){ if (config.upOnMouseOut) return up(evt); }); 
+    svg.addEventListener('mousemove', move);
+    svg.addEventListener('touchmove', move);
+    // The "wheel" event is rather new, see if it works on some older browsers (IE9 comes to mind..)
+    svg.addEventListener('wheel', wheel);
+
+    svg.addEventListener('dragstart', function(evt) {evt.preventDefault(); }); // no dragging away svg elements please
+
+};
